97a2047054e2fea32d300157aeddbc82add86e6c
Matt Burke <git@mattburkedev.com>
2016-02-11 21:53:31 +0000

    Add method for creating regex character sets

--- src/PygmentSharp/PygmentSharp.Core/Lexers/RegexUtil.cs
+++ src/PygmentSharp/PygmentSharp.Core/Lexers/RegexUtil.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace PygmentSharp.Core.Lexers
 {
@@ -48,6 +50,41 @@ namespace PygmentSharp.Core.Lexers
         {
             var passed = AllClasses.Where(c => !classes.Contains(c)).ToArray();
             return Combine(passed);
+        }
+
+        private const string CharsetEscaper = @"[\^\\\-\]]";
+
+        /// <summary>
+        /// Creates a character set that could match any of the characters in the string
+        /// </summary>
+        /// <remarks>
+        /// For example: <c>"abc"</c> would become <c>"[abc]"</c>
+        /// </remarks>
+        /// <param name="letters">THe letters to be included in the character set</param>
+        /// <returns>A regex string for the character set</returns>
+        public static string MakeCharset(string letters)
+        {
+            return "[" +
+                   Regex.Replace(letters, CharsetEscaper, m => "\\" + m.Value) +
+                   "]";
+        }
+
+        /// <summary>
+        /// Creates a character set that could match any of the characters in the list
+        /// </summary>
+        /// <remarks>
+        /// For example: <c>{ "a","b","c" }</c> would become <c>"[abc]"</c>
+        /// </remarks>
+        /// <param name="letters">THe letters to be included in the character set</param>
+        /// <returns>A regex string for the character set</returns>
+        public static string MakeCharset(params char[] letters)
+        {
+            return MakeCharset(new string(letters));
+        }
+
+        public static string Words(params string[] words)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ src/PygmentSharp/PygmentSharp.UnitTests/Lexers/RegexUtilTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NFluent;
+
+using PygmentSharp.Core.Lexers;
+
+using Xunit;
+
+namespace PygmentSharp.UnitTests.Lexers
+{
+    public class RegexUtilTests
+    {
+        [Theory]
+        [InlineData("abc", "[abc]")]
+        [InlineData("ab]", @"[ab\]]")]
+        [InlineData("^-\\]", @"[\^\-\\\]]")]
+        public void MakeCharset(string letters, string expectedRegex)
+        {
+            var result = RegexUtil.MakeCharset(letters);
+
+            Check.That(result).IsEqualTo(expectedRegex);
+        }
+
+    }
+}

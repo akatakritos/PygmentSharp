3ce042dbd6bb3a7fb7a6f9add991f1628fc0412b
Matt Burke <matt.burke@artisantechgroup.com>
2020-05-10 19:23:46 -0500

    fix fixture loading

--- src/PygmentSharp/PygmentSharp.Tests/SampleFile.cs
+++ src/PygmentSharp/PygmentSharp.Tests/SampleFile.cs
@@ -10,7 +10,7 @@ namespace PygmentSharp.UnitTests
         public static string Load(string filename)
         {
             var fullPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(),
-                "../../Fixtures/", filename));
+                "Fixtures", filename));
 
             if (!File.Exists(fullPath))
                 throw new FileNotFoundException($"Could not load fixture file {fullPath}");
